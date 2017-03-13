7e49da9bdd67eb642fdd3fe835fa06f3f843c2ff
Anthony Steele <anthony.steele@just-eat.com>
2017-03-13 13:31:37 +0000

    Simply some more after comments from stuart-lang

--- src/NLog.StructuredLogging.Json.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: Guid("b482187d-50ac-4c82-971c-e3ee284f5809")]
--- src/NLog.StructuredLogging.Json/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: Guid("9ca5ab06-26e6-48b2-a7f1-233bad75fd7e")]

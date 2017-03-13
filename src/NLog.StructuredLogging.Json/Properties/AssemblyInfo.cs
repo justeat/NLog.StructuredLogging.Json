d7beccff2bbe4900818f54dc97634dfe74fac22e
Anthony Steele <anthony.steele@just-eat.com>
2017-03-13 12:51:15 +0000

    It builds

--- src/NLog.StructuredLogging.Json/Properties/AssemblyInfo.cs
+++ src/NLog.StructuredLogging.Json/Properties/AssemblyInfo.cs
@@ -1,35 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NLog.StructuredLogging.Json")]
-[assembly: AssemblyDescription("Structured logging as JSON with NLog")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("JUST EAT plc")]
-[assembly: AssemblyProduct("NLog.StructuredLogging.Json")]
-[assembly: AssemblyCopyright("Copyright © JUST EAT plc 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9ca5ab06-26e6-48b2-a7f1-233bad75fd7e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

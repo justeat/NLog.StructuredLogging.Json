f214769179e4d60cf670818fc4e9a9d7f15e4534
Anthony Steele <anthony.steele@just-eat.com>
2017-04-03 17:12:40 +0100

    Build file needs unix line endings http://stackoverflow.com/questions/2920416/configure-bin-shm-bad-interpreter

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ export configuration=Release
 dotnet restore NLog.StructuredLogging.Json.sln --verbosity minimal || exit 1
 dotnet build src/NLog.StructuredLogging.Json/NLog.StructuredLogging.Json.csproj --output $artifacts --configuration $configuration --framework "netstandard1.3" || exit 1
 dotnet test src/NLog.StructuredLogging.Json.Tests/NLog.StructuredLogging.Json.Tests.csproj --output $artifacts --configuration $configuration --framework "netcoreapp1.0" || exit 1
+

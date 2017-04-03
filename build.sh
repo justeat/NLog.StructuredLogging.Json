379218df7964f0204481b3859ce34d397b664975
Anthony Steele <AnthonySteele@users.noreply.github.com>
2017-04-03 17:13:27 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 export artifacts=$(dirname "$(readlink -f "$0")")/artifacts
 export configuration=Release
 
-dotnet restore NLog.StructuredLogging.Json.sln --verbosity minimal || exit 1
-dotnet build src/NLog.StructuredLogging.Json/NLog.StructuredLogging.Json.csproj --output $artifacts --configuration $configuration --framework "netstandard1.3" || exit 1
-dotnet test src/NLog.StructuredLogging.Json.Tests/NLog.StructuredLogging.Json.Tests.csproj --output $artifacts --configuration $configuration --framework "netcoreapp1.0" || exit 1
-
+dotnet restore JustEat.StatsD.sln --verbosity minimal || exit 1
+dotnet build src/JustEat.StatsD/JustEat.StatsD.csproj --output $artifacts --configuration $configuration --framework "netstandard1.6" || exit 1
+dotnet test src/JustEat.StatsD.Tests/JustEat.StatsD.Tests.csproj --output $artifacts --configuration $configuration --framework "netcoreapp1.0" || exit 1

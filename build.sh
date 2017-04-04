4e9a419aa40da9c5f3fd6599207887fc102e00c8
Anthony Steele <anthony.steele@just-eat.com>
2017-04-04 09:47:55 +0100

    Don't test with nunitlite build and run

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ export configuration=Release
 
 dotnet restore NLog.StructuredLogging.Json.sln --verbosity minimal || exit 1
 dotnet build src/NLog.StructuredLogging.Json/NLog.StructuredLogging.Json.csproj --output $artifacts --configuration $configuration --framework "netstandard1.3" || exit 1
-dotnet test src/NLog.StructuredLogging.Json.Tests/NLog.StructuredLogging.Json.Tests.csproj --output $artifacts --configuration $configuration --framework "netcoreapp1.0" || exit 1
+dotnet build src/NLog.StructuredLogging.Json.Tests/NLog.StructuredLogging.Json.Tests.csproj --output $artifacts --configuration $configuration --framework "netcoreapp1.0" || exit 1
+dotnet run src/NLog.StructuredLogging.Json.Tests/NLog.StructuredLogging.Json.Tests.csproj --output $artifacts --configuration $configuration --framework "netcoreapp1.0" || exit 1

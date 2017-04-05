f93acadf00937b365e752b6649bd82ed081aa930
Anthony Steele <anthony.steele@just-eat.com>
2017-04-05 10:56:44 +0100

    null change to provoke a rebuild

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ dotnet restore NLog.StructuredLogging.Json.sln --verbosity minimal || exit 1
 dotnet build src/NLog.StructuredLogging.Json/NLog.StructuredLogging.Json.csproj --output $artifacts --configuration $configuration --framework "netstandard1.3" || exit 1
 dotnet build src/NLog.StructuredLogging.Json.Tests/NLog.StructuredLogging.Json.Tests.csproj --output $artifacts --configuration $configuration --framework "netcoreapp1.0" || exit 1
 dotnet run --project src/NLog.StructuredLogging.Json.Tests/NLog.StructuredLogging.Json.Tests.csproj --configuration $configuration --framework "netcoreapp1.0" || exit 1
+

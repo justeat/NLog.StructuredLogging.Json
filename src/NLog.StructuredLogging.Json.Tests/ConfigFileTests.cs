02db7281c3e8543b0a11d747f700a1de50729e7b
Anthony Steele <anthony.steele@just-eat.com>
2017-04-20 11:27:57 +0100

    Add a test specifically for the config file not loading It will probably fail in .Net core

---
+++ src/NLog.StructuredLogging.Json.Tests/ConfigFileTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace NLog.StructuredLogging.Json.Tests
+{
+    [TestFixture]
+    public class ConfigFileTests
+    {
+        [Test]
+        public void ThrowExceptionsFlagShouldBeRead()
+        {
+            // throwExceptions="true" should have been read from nlog.config
+            Assert.That(LogManager.ThrowExceptions, Is.True);
+        }
+    }
+}

edf7f8183b026b424b99251484c8251ddb2ca23b
Anthony Steele <anthony.steele@just-eat.com>
2017-05-06 14:39:05 +0100

    A test that fails without the new code

---
+++ src/NLog.StructuredLogging.Json.Tests/FlattenedJsonCanSerializeToFile.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLog.Config;
+using NLog.Targets;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace NLog.StructuredLogging.Json.Tests
+{
+    [TestFixture]
+    public class FlattenedJsonCanSerializeToFile
+    {
+        [Test]
+        public void JsonOutputIsCorrect()
+        {
+            var fileName = UniqueLogFileName();
+
+            var fileLogger = GetFlattenedJsonFileLogger(fileName);
+            Assert.That(fileLogger, Is.Not.Null);
+
+            fileLogger.ExtendedInfo("This is a test", 
+                new
+                {
+                    Hello = "Hello",
+                    SomeText = "this is some, text. It has! punctation?",
+                    ANumber = 42,
+                    Sometime = new DateTime(2017, 4, 5, 6, 7, 8, DateTimeKind.Utc)                    
+                });
+
+            LogManager.Flush();
+            AssertIsJsonLogEntry(fileName);
+            ClearLoggingConfig();
+        }
+
+        private void AssertIsJsonLogEntry(string fileName)
+        {
+            var json = ParseFile(fileName);
+
+            // it should be json
+            Assert.That(json, Is.Not.Null);
+            Assert.That(json.HasValues);
+
+            // standard props should be present
+            Assert.That(json.GetValue("Message"), Is.Not.Null);
+            Assert.That(json.GetValue("TimeStamp"), Is.Not.Null);
+            Assert.That(json.GetValue("Level"), Is.Not.Null);
+
+            // custom props should be present
+            Assert.That(json.GetValue("Hello"), Is.Not.Null);
+            Assert.That(json.GetValue("SomeText"), Is.Not.Null);
+            Assert.That(json.GetValue("ANumber"), Is.Not.Null);
+            Assert.That(json.GetValue("Sometime"), Is.Not.Null);
+        }
+
+        private static JObject ParseFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                Assert.Fail($"no output file exists with name {fileName}");
+            }
+
+            var contents = File.ReadAllText(fileName);
+
+            if (string.IsNullOrWhiteSpace(contents))
+            {
+                Assert.Fail($"Output file {fileName} has no  contents");
+            }
+
+            var json = JObject.Parse(contents);
+            return json;
+        }
+
+        private static string UniqueLogFileName()
+        {
+            var unique = Guid.NewGuid().ToString();
+            var date = DateTime.UtcNow.ToString("yyyyMMdd_hhmmss");
+            return $"flattenedjson_{date}_{unique}.log";
+        }
+
+        private Logger GetFlattenedJsonFileLogger(string fileName)
+        {
+            var config = new LoggingConfiguration();
+
+            var target = new FileTarget
+            {
+                Name = $"test to file {fileName}",
+                FileName = fileName,
+                Layout = new FlattenedJsonLayout()
+            };
+
+            config.AddTarget(target);
+
+            var rule = new LoggingRule("*", LogLevel.Trace, target);
+            config.LoggingRules.Add(rule);
+
+            LogManager.Configuration = config;
+            return LogManager.GetCurrentClassLogger();
+        }
+
+        private void ClearLoggingConfig()
+        {
+            LogManager.Configuration = new LoggingConfiguration();
+        }
+    }
+}

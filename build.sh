dfd3668fef390b92efc3fa899542be33b79ca7d0
Stuart Lang <stuart.b.lang@gmail.com>
2017-07-21 08:49:22 +0100

    Update to use NUnit test adapter

--- build.sh
+++ build.sh
@@ -4,6 +4,4 @@ export configuration=Release
 
 dotnet restore NLog.StructuredLogging.Json.sln --verbosity minimal || exit 1
 dotnet build src/NLog.StructuredLogging.Json/NLog.StructuredLogging.Json.csproj --output $artifacts --configuration $configuration --framework "netstandard1.3" || exit 1
-dotnet build src/NLog.StructuredLogging.Json.Tests/NLog.StructuredLogging.Json.Tests.csproj --output $artifacts --configuration $configuration --framework "netcoreapp1.0" || exit 1
-dotnet run --project src/NLog.StructuredLogging.Json.Tests/NLog.StructuredLogging.Json.Tests.csproj --configuration $configuration --framework "netcoreapp1.0" --where "!(cat == CallSite || cat == NotInNetCore)"  || exit 1
-
+dotnet test src/NLog.StructuredLogging.Json.Tests/NLog.StructuredLogging.Json.Tests.csproj --configuration Debug --framework "netcoreapp1.0" --filter "TestCategory!=CallSite&TestCategory!=NotInNetCore" || exit 1

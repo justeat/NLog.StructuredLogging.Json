455020862374faaf6a5e18491099c571f1d288a7
Anthony Steele <anthony.steele@just-eat.com>
2017-10-09 13:48:36 +0100

    update travis build script as well

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ export artifacts=$(dirname "$(readlink -f "$0")")/artifacts
 export configuration=Release
 
 dotnet restore NLog.StructuredLogging.Json.sln --verbosity minimal || exit 1
-dotnet build src/NLog.StructuredLogging.Json/NLog.StructuredLogging.Json.csproj --output $artifacts --configuration $configuration --framework "netstandard1.3" || exit 1
-dotnet test src/NLog.StructuredLogging.Json.Tests/NLog.StructuredLogging.Json.Tests.csproj --configuration Debug --framework "netcoreapp1.0" --filter "TestCategory!=CallSite&TestCategory!=NotInNetCore" || exit 1
+dotnet build src/NLog.StructuredLogging.Json/NLog.StructuredLogging.Json.csproj --output $artifacts --configuration $configuration --framework "netstandard2.0" || exit 1
+dotnet test src/NLog.StructuredLogging.Json.Tests/NLog.StructuredLogging.Json.Tests.csproj --configuration Debug --framework "netcoreapp2.0" --filter "TestCategory!=CallSite&TestCategory!=NotInNetCore" || exit 1

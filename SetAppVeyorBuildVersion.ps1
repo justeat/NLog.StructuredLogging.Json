de15704183b7f3db7373a4a844914b464255ba1c
Anthony Steele <anthony.steele@just-eat.com>
2018-01-20 17:56:44 +0000

    Swap over to JusTSaying versioning script

--- SetAppVeyorBuildVersion.ps1
+++ SetAppVeyorBuildVersion.ps1
@@ -1,112 +1,36 @@
-param(
-    [Parameter(Mandatory = $false)][switch] $RestorePackages,
-    [Parameter(Mandatory = $false)][string] $Configuration = "Release",
-    [Parameter(Mandatory = $false)][string] $VersionSuffix = "",
-    [Parameter(Mandatory = $false)][string] $OutputPath = "",
-    [Parameter(Mandatory = $false)][switch] $PatchVersion,
-    [Parameter(Mandatory = $false)][switch] $SkipTests
-)
-
-$ErrorActionPreference = "Stop"
-
-$solutionPath = Split-Path $MyInvocation.MyCommand.Definition
-$solutionFile = Join-Path $solutionPath "NLog.StructuredLogging.Json.sln"
-$libraryProject = Join-Path $solutionPath "src\NLog.StructuredLogging.Json\NLog.StructuredLogging.Json.csproj"
-$testProject = Join-Path $solutionPath "src\NLog.StructuredLogging.Json.Tests\NLog.StructuredLogging.Json.Tests.csproj"
-
-$dotnetVersion = "2.1.4"
-
-if ($OutputPath -eq "") {
-    $OutputPath = Join-Path "$(Convert-Path "$PSScriptRoot")" "artifacts"
+function Generate-RandomCharacters {
+  param (
+      [int]$Length
+  )
+  $set    = "abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray()
+  $result = ""
+  for ($x = 0; $x -lt $Length; $x++) {
+      $result += $set | Get-Random
+  }
+  return $result
 }
 
-if ($env:CI -ne $null) {
+$versionPrefix = (Select-Xml -Path ".\version.props" -XPath "/Project/PropertyGroup/VersionPrefix" | Select-Object -ExpandProperty Node).InnerText
+$versionSuffix = (Select-Xml -Path ".\version.props" -XPath "/Project/PropertyGroup/VersionSuffix" | Select-Object -First 1 -ExpandProperty Node).InnerText
+$buildNumber = $env:APPVEYOR_BUILD_NUMBER
 
-    $PatchVersion = $true
-
-    if (($VersionSuffix -eq "" -and $env:APPVEYOR_REPO_TAG -eq "false" -and $env:APPVEYOR_BUILD_NUMBER -ne "") -eq $true) {
-        $ThisVersion = $env:APPVEYOR_BUILD_NUMBER -as [int]
-        $VersionSuffix = "beta" + $ThisVersion.ToString("0000")
-    }
+if ($env:APPVEYOR_PULL_REQUEST_NUMBER){
+  $buildNumber += "-" + (Generate-RandomCharacters -Length 8)
 }
 
-$installDotNetSdk = $false;
-
-if (((Get-Command "dotnet.exe" -ErrorAction SilentlyContinue) -eq $null) -and ((Get-Command "dotnet" -ErrorAction SilentlyContinue) -eq $null)) {
-    Write-Host "The .NET Core SDK is not installed."
-    $installDotNetSdk = $true
-}
-else {
-    $installedDotNetVersion = (dotnet --version | Out-String).Trim()
-    if ($installedDotNetVersion -ne $dotnetVersion) {
-        Write-Host "The required version of the .NET Core SDK is not installed. Expected $dotnetVersion but $installedDotNetVersion was found."
-        $installDotNetSdk = $true
-    }
+if ($env:APPVEYOR_REPO_TAG -ne "true") {
+  if ($versionSuffix -ne "") {
+    $versionSuffix += "-build$buildNumber"
+  }
+  if ($versionSuffix -eq "") {
+    $versionSuffix = "build$buildNumber"
+  }
 }
 
-if ($installDotNetSdk -eq $true) {
-    $env:DOTNET_INSTALL_DIR = Join-Path "$(Convert-Path "$PSScriptRoot")" ".dotnetcli"
-
-    if (!(Test-Path $env:DOTNET_INSTALL_DIR)) {
-        mkdir $env:DOTNET_INSTALL_DIR | Out-Null
-        $installScript = Join-Path $env:DOTNET_INSTALL_DIR "install.ps1"
-        Invoke-WebRequest "https://raw.githubusercontent.com/dotnet/cli/release/2.0.0/scripts/obtain/dotnet-install.ps1" -OutFile $installScript
-        & $installScript -Version "$dotnetVersion" -InstallDir "$env:DOTNET_INSTALL_DIR" -NoPath
-    }
-
-    $env:PATH = "$env:DOTNET_INSTALL_DIR;$env:PATH"
-    $dotnet = Join-Path "$env:DOTNET_INSTALL_DIR" "dotnet.exe"
-}
-else {
-    $dotnet = "dotnet"
+if ($versionSuffix -ne "") {
+  $version = "$versionPrefix-$versionSuffix"
+} else {
+  $version = $versionPrefix
 }
 
-function DotNetRestore {
-    param([string]$Project)
-    & $dotnet restore $Project --verbosity minimal
-    if ($LASTEXITCODE -ne 0) {
-        throw "dotnet restore failed with exit code $LASTEXITCODE"
-    }
-}
-
-function DotNetPack {
-    param([string]$Project)
-
-    if ($VersionSuffix) {
-        & $dotnet pack $Project --output $OutputPath --configuration $Configuration --version-suffix "$VersionSuffix"
-    }
-    else {
-        & $dotnet pack $Project --output $OutputPath --configuration $Configuration
-    }
-    if ($LASTEXITCODE -ne 0) {
-        throw "dotnet pack failed with exit code $LASTEXITCODE"
-    }
-}
-
-function DotNetTest {
-    param([string]$Project)
-
-    & $dotnet test $Project --output $OutputPath --framework net462
-
-    if ($LASTEXITCODE -ne 0) {
-        throw "dotnet test failed with exit code $LASTEXITCODE"
-    }
-}
-
-if ($RestorePackages -eq $true) {
-    Write-Host "Restoring NuGet packages for solution..." -ForegroundColor Green
-    DotNetRestore $solutionFile
-}
-
-Write-Host "Packaging solution..." -ForegroundColor Green
-
-DotNetPack $libraryProject
-
-if ($SkipTests -eq $false) {
-    Write-Host "Running tests..." -ForegroundColor Green
-    DotNetTest $testProject
-}
-
-if ($PatchVersion -eq $true) {
-    Set-Content ".\AssemblyVersion.cs" $assemblyVersion -Encoding utf8
-}
+Update-AppveyorBuild -Version "$version"

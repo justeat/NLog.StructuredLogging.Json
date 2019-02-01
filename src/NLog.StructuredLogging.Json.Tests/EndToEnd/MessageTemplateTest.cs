c7af06ecec7f651aaecb7705ce866794ff8a374d
Anthony Steele <anthony.steele@just-eat.com>
2019-02-01 08:52:59 +0000

    And an end-to-end test

---
+++ src/NLog.StructuredLogging.Json.Tests/EndToEnd/MessageTemplateTest.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+
+namespace NLog.StructuredLogging.Json.Tests.EndToEnd
+{
+    public abstract class MessageTemplateTest : EndToEndTests
+    {
+        private string _output;
+
+        protected override void When()
+        {
+            var logInfo = new LogEventInfo
+            {
+                Level = LogLevel.Info,
+                Message = "A message with {PropA} and {PropB} embedded",
+                Parameters = new object[] {1, "two"}
+            };
+
+            Sut.Log(logInfo);
+            var lines = LogManager.Configuration.LogMessage(TargetName);
+            _output = lines[0];
+        }
+
+        [Test]
+        public void OutputHasText()
+        {
+            Assert.That(_output, Does.Contain("\"Message\":\"A message with 1 and \\\"two\\\" embedded\""));
+            Assert.That(_output, Does.Contain("\"MessageTemplate\":\"A message with {PropA} and {PropB} embedded"));
+            Assert.That(_output, Does.Contain("\"PropA\":\"1"));
+            Assert.That(_output, Does.Contain("\"PropB\":\"two"));
+            Assert.That(_output, Does.Not.Contain("Parameters"));
+        }
+    }
+}
--- src/NLog.StructuredLogging.Json.Tests/EndToEnd/ViaLayoutRenderer/MessageTemplateInLayoutRenderer.cs
+++ src/NLog.StructuredLogging.Json.Tests/EndToEnd/ViaLayoutRenderer/MessageTemplateInLayoutRenderer.cs
@@ -2,7 +2,7 @@ using NLog.Layouts;
 
 namespace NLog.StructuredLogging.Json.Tests.EndToEnd.ViaLayoutRenderer
 {
-    public class UnicodePropertiesInLayoutRenderer : UnicodePropertiesAreSerialised
+    public class MessageTemplateInLayoutRenderer : MessageTemplateTest
     {
         protected override Layout GivenLayout()
         {
